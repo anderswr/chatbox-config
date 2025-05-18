f9709ada5ed8e59e4262833f0fc3bafda559e9f7
Anders <anders@roine.no>
2025-05-18 10:56:43 +0200

    Update auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,5 @@
 export function validateLogin(username, password) {
-  const ADMIN_USER = "PIADMIN";
+  const ADMIN_USER = "boks1";
   const ADMIN_PASS = process.env.NEXT_PUBLIC_PIADMIN_PWD || ""; // hent fra Vercel miljøvariabel
   
   return username === ADMIN_USER && password === ADMIN_PASS;
